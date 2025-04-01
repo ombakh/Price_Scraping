fb823d79111c3c2749767924325563222144a480
ombakhshi <ombakh28@gmail.com>
2025-04-01 13:39:55 -0400

    Valid URL checker

--- Price_Scraping/Program.cs
+++ Price_Scraping/Program.cs
@@ -1,7 +1,21 @@
-﻿class Program
+﻿using System;
+
+namespace Price_Scraping;
+class Program
 {
     public static async Task Main(string[] args)
     {
-        
+        Console.Write("Paste Amazon Link -> ");
+        string url = Console.ReadLine();
+        // Console.WriteLine(url);
+        if (!IsValidUrl(url))
+        {
+            Console.WriteLine("Link is invalid");
+        }
+    }
+    
+    private static bool IsValidUrl(string url)
+    {
+        return Uri.TryCreate(url, UriKind.Absolute, out Uri? result) && (result.Scheme == Uri.UriSchemeHttps);
     }
 }
